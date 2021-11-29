ce5697f91990cd4ccef8bf2957613f91e7e9980d
Anna <a.serodkina@gmail.com>
2021-11-29 15:32:09 +0200

    lab4

---
+++ lab4/task1/lab4_task1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardManager manager = new CardManager();
+
+            manager["spades"] = new Spades();
+            manager["clubs"] = new Clubs();
+            manager["hearts"] = new Hearts();
+            manager["diamonds"] = new Diamonds();
+
+
+            manager["spades"].Clone("6");
+            manager["spades"].Clone("7");
+            manager["spades"].Clone("8");
+            manager["spades"].Clone("9");
+            manager["spades"].Clone("10");
+            manager["spades"].Clone("J");
+            manager["spades"].Clone("Q");
+            manager["spades"].Clone("K");
+
+            manager["hearts"].Clone("6");
+            manager["hearts"].Clone("7");
+            manager["hearts"].Clone("8");
+            manager["hearts"].Clone("9");
+            manager["hearts"].Clone("10");
+            manager["hearts"].Clone("J");
+            manager["hearts"].Clone("Q");
+            manager["hearts"].Clone("K");
+
+            manager["clubs"].Clone("6");
+            manager["clubs"].Clone("7");
+            manager["clubs"].Clone("8");
+            manager["clubs"].Clone("9");
+            manager["clubs"].Clone("10");
+            manager["clubs"].Clone("J");
+            manager["clubs"].Clone("Q");
+            manager["clubs"].Clone("K");
+
+            manager["diamonds"].Clone("6");
+            manager["diamonds"].Clone("7");
+            manager["diamonds"].Clone("8");
+            manager["diamonds"].Clone("9");
+            manager["diamonds"].Clone("10");
+            manager["diamonds"].Clone("J");
+            manager["diamonds"].Clone("Q");
+            manager["diamonds"].Clone("K");
+
+            Console.ResetColor();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+
+        abstract class Card
+        {
+            public abstract Card Clone(string r);
+        }
+
+
+        class Clubs : Card
+        {
+            string rank;
+            public override Card Clone(string r)
+            {
+                this.rank = r;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine(" ----------------");
+                Console.WriteLine("|   " + r + "  clubs     |");
+                Console.WriteLine("|            " + r + "   |");
+                Console.WriteLine(" ----------------");
+                Console.ResetColor();
+
+                return this.MemberwiseClone() as Card;
+            }
+        }
+        class Spades : Card
+        {
+            string rank;
+            public override Card Clone(string r)
+            {
+                this.rank = r;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine(" ----------------");
+                Console.WriteLine("|   " + r + "  spades    |");
+                Console.WriteLine("|            " + r + "   |");
+                Console.WriteLine(" ----------------");
+
+                return this.MemberwiseClone() as Card;
+            }
+        }
+        class Hearts : Card
+        {
+            string rank;
+            public override Card Clone(string r)
+            {
+                this.rank = r;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine(" ----------------");
+                Console.WriteLine("|   " + r + "  hearts    |");
+                Console.WriteLine("|            " + r + "   |");
+                Console.WriteLine(" ----------------");
+                Console.ResetColor();
+
+                return this.MemberwiseClone() as Card;
+            }
+        }
+        class Diamonds : Card
+        {
+            string rank;
+            public override Card Clone(string r)
+            {
+                this.rank = r;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine(" ----------------");
+                Console.WriteLine("|   " + r + " diamonds   |");
+                Console.WriteLine("|            " + r + "   |");
+                Console.WriteLine(" ----------------");
+                Console.ResetColor();
+
+                return this.MemberwiseClone() as Card;
+            }
+        }
+
+        class CardManager
+        {
+            private Dictionary<string, Card> _cards = new Dictionary<string, Card>();
+
+            // Indexer
+            public Card this[string key]
+            {
+                get { return _cards[key]; }
+                set { _cards.Add(key, value); }
+            }
+        }
+
+    }
+}
---
+++ lab4/task2/lab4_task2.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalFoodFactory factory = new CatFoodFactory();
+            factory.CreateCheapFood();
+            factory.CreateVipLuxuryFood();
+        
+            factory = new DogFoodFactory();
+            factory.CreateCheapFood();
+            factory.CreateVipLuxuryFood();
+
+            factory = new BirdFoodFactory();
+            factory.CreateCheapFood();
+            factory.CreateVipLuxuryFood();
+        
+            Console.ReadKey();
+        }
+
+        abstract class AnimalFoodFactory
+        {
+            public abstract CheapFood CreateCheapFood();
+            public abstract LuxuryFood CreateVipLuxuryFood();
+        }
+
+        class CatFoodFactory : AnimalFoodFactory
+        {
+            public override CheapFood CreateCheapFood()
+            {
+                CheapFood food = new CatCheapFood();
+                Console.WriteLine(food.message + " | price: " + food.price + "$");
+                return food;
+            }
+
+            public override LuxuryFood CreateVipLuxuryFood()
+            {
+                LuxuryFood food = new CatLuxuryFood();
+                Console.WriteLine(food.message + " | price: " + food.price + "$");
+                return food;
+            }
+        }
+
+        class BirdFoodFactory : AnimalFoodFactory
+        {
+            public override CheapFood CreateCheapFood()
+            {
+                CheapFood food = new BirdCheapFood();
+                Console.WriteLine(food.message + " | price: " + food.price + "$");
+                return food;
+            }
+
+            public override LuxuryFood CreateVipLuxuryFood()
+            {
+                LuxuryFood food = new BirdLuxuryFood();
+                Console.WriteLine(food.message + " | price: " + food.price + "$");
+                return food;
+            }
+        }
+
+        class DogFoodFactory : AnimalFoodFactory
+        {
+            public override CheapFood CreateCheapFood()
+            {
+                CheapFood food = new DogCheapFood();
+                Console.WriteLine(food.message + " | price: " + food.price + "$");
+                return food;
+            }
+
+            public override LuxuryFood CreateVipLuxuryFood()
+            {
+                LuxuryFood food = new DogLuxuryFood();
+                Console.WriteLine(food.message + " | price: " + food.price + "$");
+                return food;
+            }
+        }
+
+
+        abstract class CheapFood
+        {
+            public string message;
+            public int price = 10;
+        }
+
+        abstract class LuxuryFood
+        {
+            public string message;
+            public int price = 100;
+        }
+
+        class CatCheapFood : CheapFood
+        {
+            public CatCheapFood()
+            {
+                this.message = "cheap food for CATS";
+            }
+        }
+
+        class CatLuxuryFood : LuxuryFood
+        {
+            public CatLuxuryFood()
+            {
+                this.message = "LuXuRy food for CATS";
+            }
+        }
+
+        class BirdCheapFood : CheapFood
+        {
+            public BirdCheapFood()
+            {
+                this.message = "cheap food for BIRDS";
+            }
+        }
+
+        class BirdLuxuryFood : LuxuryFood
+        {
+            public BirdLuxuryFood()
+            {
+                this.message = "LuXuRy food for BIRDS";
+            }
+        }
+
+        class DogCheapFood : CheapFood
+        {
+            public DogCheapFood()
+            {
+                this.message = "cheap food for DOGS";
+            }
+        }
+
+        class DogLuxuryFood : LuxuryFood
+        {
+            public DogLuxuryFood()
+            {
+                this.message = "LuXuRy food for DOGS";
+            }
+        }
+
+    }
+}
