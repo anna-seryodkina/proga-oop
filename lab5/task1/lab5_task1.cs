54c4e13065c4dc7af72a698894c3275a6970e5df
Anna <a.serodkina@gmail.com>
2021-12-05 21:59:30 +0200

    lab5

---
+++ lab5/task1/lab5_task1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Student st = new Student("John Smith");
+  
+            st.Study(2);
+            st.Study(1);
+            st.Study(10);
+            st.Study(1);
+            st.PassExam();
+    
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+
+    abstract class State
+    {
+        protected Student student;
+        protected int progress;
+    
+        protected int lowerLimit;
+        protected int upperLimit;
+        protected int grade;
+    
+        // Properties
+        public Student Student
+        {
+            get { return student; }
+            set { student = value; }
+        }
+    
+        public int Progress
+        {
+            get { return progress; }
+            set { progress = value; }
+        }
+    
+        public abstract void Study(int lessons);
+        public abstract void PassExam();
+    }
+
+
+    class ZeroPointsDefaultState : State
+    {
+        public ZeroPointsDefaultState(State state) : this(state.Progress, state.Student)
+        {
+            //
+        }
+    
+        public ZeroPointsDefaultState(int progress, Student st)
+        {
+            this.progress = progress;
+            this.student = st;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            lowerLimit = 0;
+            upperLimit = 60;
+            grade = 2;
+        }
+
+        public override void PassExam()
+        {
+            Console.WriteLine($"you passed the exam. Grade: {this.grade}");
+        }
+
+        public override void Study(int lessons)
+        {
+            progress += 5 * lessons;
+            StateChangeCheck();
+        }
+
+        private void StateChangeCheck()
+        {
+            if (progress > upperLimit)
+            {
+                student.State = new ThreePointsState(this);
+            }
+        }
+    }
+
+    class ThreePointsState : State
+    {
+        public ThreePointsState(State state)
+        {
+            this.progress = state.Progress;
+            this.student = state.Student;
+            Initialize();
+        }
+
+
+        private void Initialize()
+        {
+            lowerLimit = 60;
+            upperLimit = 80;
+            grade = 3;
+        }
+
+        public override void PassExam()
+        {
+            Console.WriteLine($"you passed the exam. Grade: {this.grade}");
+        }
+
+        public override void Study(int lessons)
+        {
+            progress += 5 * lessons;
+            StateChangeCheck();
+        }
+
+        private void StateChangeCheck()
+        {
+            if (progress > upperLimit)
+            {
+                student.State = new FourPointsState(this);
+            }
+        }
+    }
+
+    class FourPointsState : State
+    {
+        public FourPointsState(State state)
+        {
+            this.progress = state.Progress;
+            this.student = state.Student;
+            Initialize();
+        }
+
+
+        private void Initialize()
+        {
+            lowerLimit = 80;
+            upperLimit = 95;
+            grade = 4;
+        }
+
+        public override void PassExam()
+        {
+            Console.WriteLine($"you passed the exam. Grade: {this.grade}");
+        }
+
+        public override void Study(int lessons)
+        {
+            progress += 5 * lessons;
+            StateChangeCheck();
+        }
+
+        private void StateChangeCheck()
+        {
+            if (progress > upperLimit)
+            {
+                student.State = new FivePointsState(this);
+            }
+        }
+    }
+
+    class FivePointsState : State
+    {
+        public FivePointsState(State state)
+        {
+            this.progress = state.Progress;
+            this.student = state.Student;
+            Initialize();
+        }
+
+
+        private void Initialize()
+        {
+            lowerLimit = 95;
+            upperLimit = 100;
+            grade = 5;
+        }
+
+        public override void PassExam()
+        {
+            Console.WriteLine($"you passed the exam. Grade: {this.grade}");
+        }
+
+        public override void Study(int lessons)
+        {
+            progress += 5 * lessons;
+            StateChangeCheck();
+        }
+
+        private void StateChangeCheck()
+        {
+            if (progress < lowerLimit)
+            {
+                student.State = new ZeroPointsDefaultState(this);
+            }
+        }
+    }
+
+
+    class Student
+    {
+        private State _state;
+        private string _name;
+    
+        // Constructor
+        public Student(string st_name)
+        {
+            this._name = st_name;
+            this._state = new ZeroPointsDefaultState(0, this);
+        }
+
+        // Properties
+        public int Progress
+        {
+            get { return _state.Progress; }
+        }
+    
+        public State State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+    
+        public void Study(int lessons)
+        {
+            _state.Study(lessons);
+            Console.WriteLine($"you are studing... [progress: {_state.Progress}% ]");
+        }
+        public void PassExam()
+        {
+            _state.PassExam();
+        }
+    }
+
+}
---
+++ lab5/task2/lab5_task2.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Handler hr1 = new Recruiter1();
+            Handler hr2 = new Recruiter2();
+
+            hr1.SetSuccessor(hr2);
+            hr1.SetSpecialists(new List<Summary>());
+            hr2.SetSpecialists(new List<Summary>());
+
+            Summary mrCodersSummary = new Summary("Coder13", 21, 3, 3000, "nothing", "dev");
+            hr1.AddSpecialist(mrCodersSummary);
+
+
+            Summary mrJsSummary = new Summary("Joe", 25, 4, 10000, "java something", "frontend developer");
+            hr2.AddSpecialist(mrJsSummary);
+
+
+            Vacancy vacancy = new Vacancy(4, 12000, "frontend developer");
+            hr1.ProcessRequest(vacancy);
+        }
+    }
+
+
+    abstract class Handler
+    {
+        protected Handler successor;
+        protected List<Summary> specialists;
+ 
+        public void SetSuccessor(Handler successor)
+        {
+            this.successor = successor;
+        }
+
+        public void SetSpecialists(List<Summary> specialists)
+        {
+            this.specialists = specialists;
+        }
+    
+        public abstract void ProcessRequest(Vacancy vacancy);
+
+        public abstract void AddSpecialist(Summary summary);
+
+    }
+
+    class Recruiter1 : Handler
+    {
+        public override void ProcessRequest(Vacancy vacancy)
+        {
+            if (specialists != null)
+            {
+                foreach(Summary s in specialists)
+                {
+                    if(s.position == vacancy.Position && s.experience == vacancy.Experience)
+                    {
+                        Console.WriteLine($"\nCandidate: {s.fullname}, {s.age} y.o.\n");
+                        return;
+                    }
+                }
+            }
+
+            if (successor != null)
+            {
+                successor.ProcessRequest(vacancy);
+            }
+        }
+
+        public override void AddSpecialist(Summary summary)
+        {
+            this.specialists.Add(summary);
+        }
+    }
+
+    class Recruiter2 : Handler
+    {
+        public override void ProcessRequest(Vacancy vacancy)
+        {
+            if (specialists != null)
+            {
+                foreach(Summary s in specialists)
+                {
+                    if(s.position == vacancy.Position && s.experience == vacancy.Experience)
+                    {
+                        Console.WriteLine($"\nCandidate: {s.fullname}, {s.age} y.o.\n");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nот халепа! нікого не знайшли...(\n");
+        }
+
+        public override void AddSpecialist(Summary summary)
+        {
+            this.specialists.Add(summary);
+        }
+    }
+
+    class Vacancy
+    {
+        private int _experience;
+        private double _salary;
+        private string _position;
+
+        public Vacancy(int experience, double salary, string pos)
+        {
+            this._experience = experience;
+            this._salary = salary;
+            this._position = pos;
+        }
+    
+        public int Experience
+        {
+
+            get { return _experience; }
+            set { _experience = value; }
+        }
+    
+        public double Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+    
+        public string Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+    }
+
+    class Summary
+    {
+        public string fullname;
+        public int age;
+        public int experience;
+        public double salary;
+        public string skills;
+        public string position;
+
+        public Summary(string fullname, int age, int experience, double salary, string skills, string position)
+        {
+            this.fullname = fullname;
+            this.age = age;
+            this.experience = experience;
+            this.salary = salary;
+            this.skills = skills;
+            this.position = position;
+        }
+    }
+
+}
